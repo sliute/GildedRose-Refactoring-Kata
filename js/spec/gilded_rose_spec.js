a4ba3a7c6afa66e3e27cd00e5e23f76081c27965
Stefan Liute <stefanliute@gmail.com>
2017-03-07 16:21:45 +0000

    Test for requirements; TDD 'conjured' items
    Why:
    
    * There were no tests in place to check whether the existing codebase
    will continue to perform normally after I start adding to it.
    * The existing codebase is not written in an ideal fashion (multiple
    nested ifs are hard to follow).
    
    This change addresses the need by:
    
    * Writing tests for each requirement the kata specifies.
    * Writing a test for the 'conjured' items, then add a minimal amount of
    code to make it pass.

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,81 @@
 describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    const gilgedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gilgedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  it ("should have standard items, whose quality decreases in time (twice as fast after sell-by date)", function() {
+    const gildedRose = new Shop([
+      new Item("Foo Fine Fodder", 5, 10),
+      new Item("Foo Fine Fodder", -1, 5)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(9);
+    expect(items[1].quality).toEqual(3);
   });
 
+  it ("should never have item quality drop below 0", function() {
+    const gildedRose = new Shop([
+      new Item("Foo Fine Fodder", -3, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert", -3, 0)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+    expect(items[1].quality).toEqual(0);
+  });
+
+  it ("should have Aged Brie, whose quality increases in time", function() {
+    const gildedRose = new Shop([
+      new Item("Aged Brie", 5, 20),
+      new Item("Aged Brie", -2, 20)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(21);
+    expect(items[1].quality).toEqual(22);
+  });
+
+  it ("should never have item quality rise above 50", function() {
+    const gildedRose = new Shop([
+      new Item("Aged Brie", -30, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 2, 50)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(50);
+    expect(items[1].quality).toEqual(50);
+  });
+
+  it ("should have Sulfuras, whose quality never alters", function() {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(80);
+    expect(items[1].quality).toEqual(80);
+  });
+
+  it ("should have backstage passes, whose quality increases until the concert date", function() {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(11);
+    expect(items[1].quality).toEqual(12);
+    expect(items[2].quality).toEqual(13);
+  });
+
+  it ("should have backstage passes, whose quality drops to 0 after the concert date", function() {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 15)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("should have Conjured items whose quality decreases twice as fast (before and after sell-by date)", function() {
+    const gildedRose = new Shop([
+      new Item("Conjured goblin earwax", 3, 10),
+      new Item("Conjured goblin earwax", -3, 6)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(8);
+    expect(items[1].quality).toEqual(2);
+  });
 });
